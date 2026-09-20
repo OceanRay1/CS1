e89bc65fd9512421ed53f468b50fbe470938e991
OceanRay1 <roshan.shivnani@gmail.com>
2026-09-20 16:18:27 -0400

    Func Pset 1

---
+++ SA2/FuncPrac.py
@@ -0,0 +1,49 @@
+# Author: Roshan Shivnani
+# Date: 09/20/2026
+# Purpose: FunctionPSet
+
+#1
+def Square(n):
+    print(n * n)
+
+#2
+def Sum(m, n):
+    print(m + n)
+
+#3
+def Product(m, n):
+    print(m * n)
+
+def Combo(m, n):
+    Sum(m, n)
+    Product(m, n)
+
+#4
+#It will print 25 on line 1, then 115 on line 2
+
+#5
+#It will just show an error that Func2 is undefined
+
+#6
+#It will print "in func1" on line 1, (6, 4) as ints on line 2, then 5.0 as float on line 3
+
+#8
+#C - Syntax Error
+
+#9 Swap
+a = 10
+b = 20
+c = 0
+print(a, b)
+c = a
+a = b
+b = c
+print(a, b)
+
+#10
+#PRINTS
+#in func 2
+#16
+
+#in func 2.5
+#25
